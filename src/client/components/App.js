14f21416137479dcaa181ad823341ed74672910e
spencel <spence.lank@gmail.com>
2018-06-24 16:42:19 -0700

    m

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AddTodoContainer from '../containers/AddTodoContainer'
 import TodoListContainer from '../containers/TodoListContainer'
+import Mom from './Mom'
 
 
 const App = () => (
   <div className='App'>
+    <Mom />
     <AddTodoContainer />
     <TodoListContainer />
   </div>
